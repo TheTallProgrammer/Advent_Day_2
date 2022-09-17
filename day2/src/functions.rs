494d4679a65434a9c88e0493db63850b5fcdcf07
Logan Falkenberg <loganf0101@gmail.com>
2022-09-17 14:00:01 -0500

    changed hPos to _h_pos

--- day2/src/functions.rs
+++ day2/src/functions.rs
@@ -1,7 +1,7 @@
 
 
 pub fn instantiator(){ // Head function
-    let (mut _depth, mut _hPos) = (0,0);
+    let (mut _depth, mut _h_pos) = (0,0);
 } // End of instantiator
 
 pub fn read_file(){
@@ -12,13 +12,13 @@ pub fn forward(){ // Moves forward changing the hPos by input
     todo!();
 } // End of forward
 
-pub fn down(){ // Moves down changing the depth by input
-    todo!();
-} // End of down
-
 pub fn up(){ // Moves up changing the depth by input
     todo!();
 } // End of up
+
+pub fn down(){ // Moves down changing the depth by input
+    todo!();
+} // End of down
 
 pub fn calclations(){ //multiply the depth * hPos to get final position
     todo!();
