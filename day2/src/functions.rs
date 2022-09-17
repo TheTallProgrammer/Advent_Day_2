ef8775d63feb3417c4d8c6d36f96d79e376d1692
Logan Falkenberg <loganf0101@gmail.com>
2022-09-17 14:25:38 -0500

    read in file data.txt. Use nested loop to first loop through each line, then the inner loop iterates through each string in that line. Each line has two strings. The first string if it is a direction, is put into directions vector and converted to string, the second string in the line is converted to i32 if it is a number and put into shifts vector. Will use these as parallel arrays to figure out the exact shifts made from the inputs

--- day2/src/functions.rs
+++ day2/src/functions.rs
@@ -1,11 +1,29 @@
-
+// Libraries
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::str::FromStr;
 
 pub fn instantiator(){ // Head function
     let (mut _depth, mut _h_pos) = (0,0);
+    read_file();
 } // End of instantiator
 
 pub fn read_file(){
-    todo!();
+    let file = File::open("src/data.txt").unwrap();
+    let reader = BufReader::new(file);
+    let mut directions:Vec<String> = Vec::new(); // Vector to hold directions from data file
+    let mut shifts:Vec<i32> = vec![0i32]; // Vector to contain the shifts of each direction from data file
+    for (_index,line) in reader.lines().enumerate() { // loop that iterates through the data file line by line
+        let line = line.unwrap(); // line is each line in data.txt
+        for substr in line.split_whitespace(){ // Puts each word in data file into its own line
+            if substr == "forward" || substr == "down" || substr == "up" { // If the input data is a direction, add it to the directions vector
+                directions.push(substr.to_string()); // Converts &str to string
+            } else {
+                let temp_str:i32 = FromStr::from_str(substr).unwrap(); // Converts from &str to i32
+                shifts.push(temp_str);
+            }
+        } // End of inside loop
+    } // End of outside loop
 } // End of read_file
 
 pub fn forward(){ // Moves forward changing the hPos by input
--- day2/target/debug/day2.d
+++ day2/target/debug/day2.d
@@ -1,1 +1,1 @@
-C:\Users\Logan\PrivateContainer\rust\advent2022\day2\day2\target\debug\day2.exe: C:\Users\Logan\PrivateContainer\rust\advent2022\day2\day2\src\main.rs
+C:\Users\Logan\PrivateContainer\rust\advent2022\day2\day2\target\debug\day2.exe: C:\Users\Logan\PrivateContainer\rust\advent2022\day2\day2\src\functions.rs C:\Users\Logan\PrivateContainer\rust\advent2022\day2\day2\src\main.rs
