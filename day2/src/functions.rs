d03e50b484689e4d8d78f904bc55ee50ea057056
Logan Falkenberg <loganf0101@gmail.com>
2022-09-18 00:41:05 -0500

    removed unused code. Had to create seperate iterator for the shifts vector because the initial element in the vector was set to 0, couldn't fix it, so just start an element later. Also flipped addition/subtraction operators when comparing if the direction was up/down.

--- day2/src/functions.rs
+++ day2/src/functions.rs
@@ -5,43 +5,36 @@ use std::str::FromStr;
 
 pub fn instantiator(){ // Head function
     let (mut _depth, mut _h_pos) = (0,0);
-    let (directions, shifts) = read_file(); // Calls read_file and returns the input that would caught from the data.txt
+    let (directions, shifts) = read_file(); // Calls read_file function and returns the input that would caught from the data.txt
     determiner(&directions, &shifts, &mut _depth, &mut _h_pos); // uses pass by reference for each argument to avoid returning 
     calclations(_depth, _h_pos);
 } // End of instantiator
 
-pub fn read_file() -> (Vec<String>, Vec<i32>){
+pub fn read_file() -> (Vec<String>, Vec<i32>){ // Returns directions, _shifts vectors
     let file = File::open("src/data.txt").unwrap();
     let reader = BufReader::new(file);
-    let mut directions:Vec<String> = Vec::new(); // Vector to hold directions from data file
-    let mut shifts:Vec<i32> = vec![0i32]; // Vector to contain the shifts of each direction from data file
+    let (mut directions, mut _shifts) = (Vec::new(),vec![0i32]); // Vectors that will hold either directions or shifts
     for (_index,line) in reader.lines().enumerate() { // loop that iterates through the data file line by line
         let line = line.unwrap(); // line variable is each line in data.txt
-        for substr in line.split_whitespace(){ // Puts each word before a whitespace in data file into its own line
-            if substr == "forward" || substr == "down" || substr == "up" { // If the input data is a direction, add it to the directions vector
-                directions.push(substr.to_string()); // Converts &str to string
-            } else { // if not direction
-                let temp_str:i32 = FromStr::from_str(substr).unwrap(); // Converts from &str to i32
-                shifts.push(temp_str);
-            }
-            println!("Direction: {}, hPos: {}", directions[i], shifts[i]);
+        for substr in line.split_whitespace(){ // Puts each word before a whitespace inside of the data file into its own line
+            if substr == "forward" || substr == "down" || substr == "up" {directions.push(substr.to_string());} // Converts &str to string
+            else {_shifts.push(FromStr::from_str(substr).unwrap());} // Converts from &str to i32
         } // End of inside loop
     } // End of outside loop
-    return (directions, shifts);
+    return (directions, _shifts);
 } // End of read_file
 
 pub fn determiner(directions: &Vec<String>, shifts: &Vec<i32>, _depth: &mut i32, _h_pos: &mut i32 ){ // Use pass by reference to not have to return anything, just updates the vectors if anything needs to change  
-    for i in 1..10 {
+    let mut _shifts_i = 1; // Have to set to 1 because the first value is 0 for some reason
+    for i in 0..directions.len(){
         let _item = &directions[i].to_string();
-        if directions[i] == "forward"{(*_h_pos) += shifts[i]} // Dereference the _depth with *
-        if directions[i] == "up"{(*_depth) += shifts[i];} 
-        if directions[i] == "down"{(*_depth) -= shifts[i];}
-        
+        if directions[i] == "forward"{(*_h_pos) += shifts[_shifts_i]} // Dereference the _depth with*
+        if directions[i] == "up"{(*_depth) -= shifts[_shifts_i];} 
+        if directions[i] == "down"{(*_depth) += shifts[_shifts_i];}
+        _shifts_i += 1;
     } // End of loop
 } // End of determiner 
 
 pub fn calclations(_depth: i32, _h_pos: i32){ //multiply the depth * hPos to get final position
     println!("Depth: {}, hPos: {}, Total Shift: {} ", _depth, _h_pos, (_depth * _h_pos));
 } // End of calculations
-
-// need to figure out how to make the first hPos index not be 0 in the loop on line 18, pushing 0 into it makes the entire vector behind by one.
